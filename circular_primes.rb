2f8612921dcecc1b443f247dd3de457fa8e16423
Larkin Piner <lpiner@cisco.com>
2015-10-22 12:01:26 -0400

    Turns out I don't know the diffrence between rotation and permutations....... Problem solved

--- circular_primes.rb
+++ circular_primes.rb
@@ -1,4 +1,7 @@
 require 'benchmark'
+def rotate(i)
+	i.push i.shift
+end
 def is_prime?(n)
 	x = 1
 	divisors = []
@@ -6,24 +9,20 @@ def is_prime?(n)
 		if n % x == 0
 			divisors << x
 			divisors << n/x
+			#Much faster to break and return the moment that we know there's more than 2 divisors
+			break if divisors.count > 2
 		end	
 		x +=1
 	end
 	
 	divisors.count == 2
 end
-cp = []
-p Benchmark.measure{
-(1..10000).each do |x|
-	n = x.to_s.split('').permutation.to_a
+def circle_prime?(prime)
+	n = prime.to_s.split('')
 	all_prime = false
-	next if x.to_s.include? "0"
-	next if x.to_s.include? "2"
-	next if x.to_s.include? "4"
-	next if x.to_s.include? "5"
-	next if x.to_s.include? "6"
-	next if x.to_s.include? "8"
-	n.each do |k|
+	n.count.times do
+		n = rotate(n)
+		k = n
 		k = k.join.to_i
 		if !is_prime?(k)
 			all_prime = false
@@ -31,8 +30,32 @@ p Benchmark.measure{
 		end	
 		all_prime = true
 	end	
-	if all_prime; cp << x end
+	all_prime
 end
+cp = []
+p "Number prune"
+p Benchmark.measure{
+numbers = []
+(1..1000000).each do |i|
+	next if i % 2 == 0
+	next if i % 5 == 0
+	numbers << i
+
+end
+p "Testing if prime"
+cp << 2
+numbers.each do |x|
+	if is_prime?(x)
+		cp << x
+	end
+end
+c_primes = []
+p "Testing for circular primes"
+cp.each do |prime|
+	if circle_prime?(prime)
+		c_primes << prime
+	end	
+end
+p c_primes.count
+
 }
-p cp
-p cp.count
