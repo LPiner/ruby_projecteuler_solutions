18f12cbad3d98e1ca80bfd26904ad0a9e35060af
Larkin Piner <lpiner@cisco.com>
2016-05-07 10:36:09 -0400

    43 done

--- coin_sums.rb
+++ coin_sums.rb
@@ -1,23 +1,13 @@
-def main(sum)
-	total = 0
-	value = [200,100,50,20,10,5,2,1]
-	value.each do |x|
-		value.each do |k|
-			p x / k
-			if x % k == 0
-				total +=1
-			end		
-		end
-	end
-	p total
-	
+values = [200,100,50,20,10,5,2,1]
+uniq_ways = 0
+target_value = 200
+
+
+values.each_with_index do |value, index|
+
+
+
 end
-def value_of_coins(array)
-	result = 0
-	value = [1,2,5,10,20,50,100,200]
-	8.times do |x|
-		result += array[x] * value[x]	
-	end
-	result
-end
-p main(10)
+
+
+
